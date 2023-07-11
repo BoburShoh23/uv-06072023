4ce412653fc0f38df3739696bedbe99adb3a7602
Bobur Nuriddinov <bbbb.bobur.nuriddinov@gmail.com>
2023-07-11 17:43:43 +0500

    begin

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let minutes = document.querySelector('.minutes')
+let seconds = document.querySelector('.seconds')
+let controlButton = document.querySelector('.controlButton')
+let some = 0
+
+controlButton.addEventListener('click', ()=>{
+    if(controlButton.innerText==='pause'){
+        some=1
+        controlButton.innerText='start'
+    }
+    else if(controlButton.innerText==='restart'){
+        some=0
+        controlButton.innerText='pause'
+    }
+    else if(controlButton.innerText==='start'){
+        some=0
+        controlButton.innerText='pause'
+    }
+    restart()    
+})
+
+function addZero(num){
+    num<10 && num>=0 ? num='0'+num : ''
+    return num
+}
+function restart(){
+        let timer = setInterval(()=>{
+        if(some===1){
+            clearInterval(timer)
+            return 0
+        }
+        let second = Number(seconds.innerText)
+        let minute = Number(minutes.innerText)
+        second--
+        if(second===(-1)){
+            second=59
+            minute--
+            minutes.innerText=minute
+        }
+        seconds.innerText=addZero(second)
+    }, 1000)
+}
